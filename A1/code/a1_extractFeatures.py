1bad63dfb2bc9a9e9c48bf2aae4a85239e05ed04
yu li <durant.li@mail.utoronto.ca>
2018-01-23 17:03:23 -0500

    initial commit for ft lots of problems

--- A1/code/a1_extractFeatures.py
+++ A1/code/a1_extractFeatures.py
@@ -3,7 +3,11 @@ import sys
 import argparse
 import os
 import json
+import string
 
+'''
+Load supported file
+'''
 #fp_path = '/u/cs401/Wordlists/First-person'
 dev_fp_path = '../Wordlists/First-person'
 fp_file = open(dev_fp_path)
@@ -14,11 +18,29 @@ dev_sp_path = '../Wordlists/Second-person'
 sp_file = open(dev_sp_path)
 sp = sp_file.readlines()
 sp = [i.replace("\n","") for i in sp]
-#Tp_path = '/u/cs401/Wordlists/Third-person'
+#tp_path = '/u/cs401/Wordlists/Third-person'
 dev_tp_path = '../Wordlists/Third-person'
 tp_file = open(dev_tp_path)
 tp = tp_file.readlines()
 tp = [i.replace("\n","") for i in tp]
+#slang_path = '/u/cs401/Wordlists/Slang'
+dev_slang_path = '../Wordlists/Slang'
+slang_file = open(dev_slang_path)
+slang = slang_file.readlines()
+slang = [i.replace("\n","") for i in slang]
+
+#slang_path = '/u/cs401/Wordlists/Slang'
+dev_slang_path = '../Wordlists/Slang'
+slang_file = open(dev_slang_path)
+slang = slang_file.readlines()
+slang = [i.replace("\n","") for i in slang]
+
+
+#some helpful tags for future regex
+common_nouns = ['NN', 'NNS']
+proper_nouns = ['NNP', 'NNPS']
+adverbs = ['RB', 'RBR', 'RBS']
+wh = ['WDT', 'WP', 'WP$', 'WRB']
 
 
 
@@ -31,17 +53,94 @@ def extract1( comment ):
     Returns:
         feats : numpy Array, a 173-length vector of floating point features
     '''
+
     feats = np.zeros(173)
     comment = comment.split(" ")
+    len_comment = 0.0
+    no_of_token = 0.0
+    no_of_sen = 0.0
+
+
     for i in comment:
-        if i in fp:
+        #some feature need to calculate regardless of words
+        len_comment += len(i)
+        no_of_token +=1
+        if '.\n' in i:
+            no_of_sen += 1
+            continue
+
+        #need to think a better way but should be enough for now
+        if fp+'/PRP' in i or fp+'/PRP$':
+            feats[0] +=1
+            continue
+        if sp in i:
             feats[1] +=1
             continue
-        if i in sp:
+        if tp in i :
             feats[2] +=1
             continue
-        if i in tp:
+        if i.find('/CC'):
             feats[3] +=1
+            continue
+        if i.find('/VBD'):
+            feats[4] +=1
+            continue
+        if i.find('/VBG'):
+            feats[5] +=1
+            continue
+        if i == ',/,':
+            feats[6] +=1
+            continue
+        if i[0] in string.punctuation and i[1] in string.punctuation:
+            feats[7] +=1
+            continue
+        if any(n in i for n in common_nouns):
+            feats[8] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(ad in i for ad in adverbs):
+            feats[10] +=1
+        if any(w in i for w in wh):
+            feats[11] +=1
+        if any(sl in i for sl in slang):
+            feats[12] +=1
+        if len(i.split('/')[0])>=3 and i.split('/')[0].isupper():
+            feats[13] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+        if any(pn in i for pn in proper_nouns):
+            feats[9] +=1
+
+
+    #average length of sentence,tokens
+    feats[14] = no_of_token/no_of_sen
+    feats[15] = len_comment/no_of_token
+    feats[16] = no_of_sen
+
 
 
     return feats
--- A1/code/test_extra.py
+++ A1/code/test_extra.py
@@ -1,14 +1,14 @@
 import unittest
 from a1_extractFeatures import extract1
-import numpy
+import numpy as np
 
-
+#to-do add more tests AL
 class test_extrac(unittest.TestCase):
 
-    def check_formated_output(self):
+    def test_formated_output(self):
         comment = 'I/PRP I/PRP'
-        self.assertIsInstance(extract1(comment),numpy.ndarray)
-        self.assertEqual(extract1(comment),"sdsds")
+        self.assertIsInstance(extract1(comment),np.ndarray)
+        self.assertEqual(extract1(comment),np.array([1,2]))
 
 
 if __name__ == '__main__':
