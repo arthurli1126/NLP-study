4a06d53f8477ea8d9900971e508ddf2db1895b76
yu li <durant.li@mail.utoronto.ca>
2018-02-08 16:41:10 -0500

    add multiprocess to part1

--- A1/code/a1_preproc.py
+++ A1/code/a1_preproc.py
@@ -1,21 +1,23 @@
-import sys
 import argparse
-import os
-import json
-import random
 import html
+import json
+import os
+import random
 import re
 import string
+import sys
 import spacy
+from multiprocessing.pool import ThreadPool
 
-#todo: need to implement multithreading
 
-#indir = '/u/cs401/A1/data/';
-#sw_path = 'u/cs401/Wordlists/StopWords';
-#ab_path = 'u/cs401/Wordlists/abbrev.english';
+# todo: need to implement multithreading
+
+# indir = '/u/cs401/A1/data/';
+# sw_path = 'u/cs401/Wordlists/StopWords';
+# ab_path = 'u/cs401/Wordlists/abbrev.english';
 dev_dir = '../data/';
-dev_sw_path ='../Wordlists/StopWords';
-dev_ab_path ='../Wordlists/abbrev.english';
+dev_sw_path = '../Wordlists/StopWords';
+dev_ab_path = '../Wordlists/abbrev.english';
 
 # load abbrevs file
 abbrev_file = open(dev_ab_path)
@@ -28,12 +30,13 @@ st_words = st_words_file.readlines()
 st_words_file.close()
 st_words = [i.replace("\n", "") for i in st_words]
 
-#create nlp
+# create nlp
 nlp = spacy.load('en', disable=['parse', 'ner'])
-punctuation = re.sub(r"[']",'',string.punctuation)
+punctuation = re.sub(r"[']", '', string.punctuation)
 
-#TODO： check end of line step
-def preproc1(comment, steps=range(1,11)):
+
+# TODO： check end of line step
+def preproc1(comment, steps=range(1, 11)):
     ''' This function pre-processes a single comment
 
     Parameters:                                                                      
@@ -55,7 +58,7 @@ def preproc1(comment, steps=range(1,11)):
     if 3 in steps:
         comment = remove_urls(comment)
     if 4 in steps:
-        comment = split_punctuation(comment,abbrevs)
+        comment = split_punctuation(comment, abbrevs)
     if 5 in steps:
         pass
     if 6 in steps:
@@ -63,14 +66,14 @@ def preproc1(comment, steps=range(1,11)):
         comment = modComm_6
     if 7 in steps:
         if 8 not in steps:
-            comment = remove_sd(comment,st_words)
+            comment = remove_sd(comment, st_words)
     if 8 in steps:
         if 6 in steps in steps:
             comment = modComm_8
         else:
             modComm_6, modComm_8 = spacy_support(comment)
             if 7 in steps:
-                comment = remove_sd(modComm_8,st_words)
+                comment = remove_sd(modComm_8, st_words)
             else:
                 comment = modComm_8
     if 9 in steps:
@@ -81,39 +84,52 @@ def preproc1(comment, steps=range(1,11)):
     modComm = comment
     return modComm
 
+
 '''
 remove new line 
 '''
+
+
 def remove_newline(comment):
-    return re.sub("\n"," ",comment)
+    return re.sub("\n", " ", comment)
+
 
 '''
 replace hmtl code with ascii
 '''
+
+
 def replace_html_code(comment):
     return html.unescape(comment)
+
 
 '''
 remove urls
 '''
+
+
 def remove_urls(comment):
     pattern = re.compile(r'(http[^\s]*|www[^\s]*)')
-    comment = re.sub(pattern,'', comment)
+    comment = re.sub(pattern, '', comment)
     return comment
+
 
 '''
 split_punctuation
 '''
-def split_punctuation(comment,abbrevs):
-    #change the period of abbrev to magic=xeq this is bad
+
+
+def split_punctuation(comment, abbrevs):
+    # change the period of abbrev to magic=xeq this is bad
     comment = re.sub(r"(\b)(" + "|".join(abbrevs) + r")",
-                     lambda m: m.group(1) + m.group(2).replace(".","xeqxeq"), comment)
+                     lambda m: m.group(1) + m.group(2).replace(".", "xeqxeq"), comment)
     comment = re.sub(r"(\w)([{}])".format(punctuation), '\g<1> \g<2> ', comment).replace("xeqxeq", ".")
-    comment = re.sub(r"\s+",' ',comment)
+    comment = re.sub(r"\s+", ' ', comment)
     return comment
 
+
 def split_clitics(comment):
- #clitics list will be sub
+    # clitics list will be sub
     c_list = ['\'s',
               '\'re',
               '\'ve',
@@ -125,45 +141,70 @@ def split_clitics(comment):
               '\'em',
               'y\'',
               'Y\'']
-    pattern = re.compile(r'\b(\w*)(' +r"|".join(c_list)+r")(\w*)\b")
+    pattern = re.compile(r'\b(\w*)(' + r"|".join(c_list) + r")(\w*)\b")
     comment = re.sub(pattern,
-                     lambda m: m.group(1) +" "+ m.group(2) +" "+m.group(3),comment)
+                     lambda m: m.group(1) + " " + m.group(2) + " " + m.group(3), comment)
     return comment
+
 
 '''
 step6&8 spacy support 
 '''
+
+
 def spacy_support(comment):
     modcom_6 = ''
     modcom_8 = ''
     utt = nlp(comment)
-    #to-do AL if token start with - dont lemmanize it
+    # to-do AL if token start with - dont lemmanize it
     for token in utt:
         modcom_6 = modcom_6 + " " + token.text + "/" + token.tag_
-        #modcom_8 = modcom_8 + " " + token.lemma_ + "/" + token.tag_
-        modcom_8 = modcom_8 + " " + (token.lemma_ if token.lemma_[0]!='-' else token.text) + "/" + token.tag_
+        # modcom_8 = modcom_8 + " " + token.lemma_ + "/" + token.tag_
+        modcom_8 = modcom_8 + " " + (token.lemma_ if token.lemma_[0] != '-' else token.text) + "/" + token.tag_
     # AL-first character was space
-    return modcom_6[1:],modcom_8[1:]
+    return modcom_6[1:], modcom_8[1:]
 
 
 def remove_sd(comment, st_words):
-    pattern = re.compile(r"\b("+r"|".join(st_words)+r")(\/\S*)?\b")
-    comment = re.sub(pattern,'', comment+' ')
+    pattern = re.compile(r"\b(" + r"|".join(st_words) + r")(\/\S*)?\b")
+    comment = re.sub(pattern, '', comment + ' ')
     comment = re.sub(r"\s+", ' ', comment)
     return comment
 
+
 def add_newline_eos(comment):
-    return re.sub(r"(\w)([.])",'\g<1>\g<2>\n', comment)
+    return re.sub(r"(\w)([.])", '\g<1>\g<2>\n', comment)
 
-#this one is little tricky
+
+# this one is little tricky
 def convert_lc(comment):
-    return re.sub(r"(\b\S+)(/\S+\b)",lambda m: m.group(1).lower() + m.group(2), comment)
+    return re.sub(r"(\b\S+)(/\S+\b)", lambda m: m.group(1).lower() + m.group(2), comment)
 
+
+'''
+Going to implement multithreading 
+for this module to speed up the process
+'''
+
+
+def process_wrapper(data, file, steps=range(0, 10)):
+    comments = []
+    # TODO: need to remove this in the furture
+    h = 0
+    for line in data:
+        j = json.loads(line)
+        d = {key: value for (key, value) in j.items() if key in ('id', 'body')}
+        d["cat"] = file
+        d["body"] = preproc1(d["body"], steps)
+        comments.append(d)
+        h += 1
+        print("{}:{}".format(file,h))
+    return comments
 
 
 def main(args):
-
     allOutput = []
+    thread_pool = ThreadPool(4)
 
     for subdir, dirs, files in os.walk(dev_dir):
         for file in files:
@@ -173,30 +214,33 @@ def main(args):
             data = json.load(open(fullFile))
 
             # TODO: select appropriate args.max lines
-            start_index = args.ID[0]%len(data)
-            print("start index : %s" %start_index)
+            start_index = args.ID[0] % len(data)
+            print("start index : %s" % start_index)
             data = data[start_index:]
-            data = [data[i] for i in random.sample(range(len(data)),int(args.max))]
-            print("new data size %s" %len(data))
-            # TODO: read those lines with something like `j = json.loads(line)`
-            # TODO: choose to retain fields from those lines that are relevant to you
-            # TODO: add a field to each selected line called 'cat' with the value of 'file' (e.g., 'Alt', 'Right', ...) 
-            # TODO: process the body field (j['body']) with preproc1(...) using default for `steps` argument
-            # TODO: replace the 'body' field with the processed text
-            # TODO: append the result to 'allOutput'
-            h = 0
-            for line in data:
-                j = json.loads(line)
-                d = {key:value for (key, value) in j.items() if key in ('id', 'body')}
-                d["cat"] = file
-                d["body"] = preproc1(d["body"])
-                allOutput.append(d)
-                h+=1
-                print(h)
+            data = [data[i] for i in random.sample(range(len(data)), int(args.max))]
+            print("new data size %s" % len(data))
+            allOutput.append(thread_pool.apply_async(process_wrapper, (data,file)))
+            # h = 0
+            # for line in data:
+            #     j = json.loads(line)
+            #     d = {key:value for (key, value) in j.items() if key in ('id', 'body')}
+            #     d["cat"] = file
+            #     d["body"] = preproc1(d["body"])
+            #     allOutput.append(d)
+            #     h+=1
+            #     print(h)
 
+    results=[]
+    for r in allOutput:
+        results += r.get()
+
+    print("dang")
     fout = open(args.output, 'w')
-    fout.write(json.dumps(allOutput))
+    fout.write(json.dumps(results))
     fout.close()
+    thread_pool.close()
+    thread_pool.join()
+
 
 if __name__ == "__main__":
 
@@ -212,4 +256,3 @@ if __name__ == "__main__":
         sys.exit(1)
 
     main(args)
-
