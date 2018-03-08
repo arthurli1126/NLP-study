03c281f65e29fcbe7fa231bb9026c8977d5f9397
yu li <durant.li@mail.utoronto.ca>
2018-03-07 23:55:37 -0500

    working on task4

--- A2/code/align_ibm1.py
+++ A2/code/align_ibm1.py
@@ -4,6 +4,25 @@ from preprocess import *
 from math import log
 import os
 
+def read_lan_file(train_dir, num_sen, lan):
+    curr_num_sen = 0
+    ret_words = {}
+    for subdir, dirs, files in os.walk(train_dir):
+        for file in files:
+            if "."+lan in file:
+                fullFile = os.path.join(subdir, file)
+                print("Peprocessing " + fullFile)
+                fp = open(fullFile)
+                # preocess each sentence
+                for line in fp:
+                    outsen = preprocess(line.strip(), lan)
+                    ret_words[curr_num_sen] = outsen.split()
+                    curr_num_sen += 1
+                    if curr_num_sen==num_sen:
+                        return ret_words
+
+
+
 def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
     """
 	Implements the training of IBM-1 word alignment algoirthm. 
@@ -15,7 +34,7 @@ def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
 	num_sentences : (int) the maximum number of training sentences to consider
 	max_iter : 		(int) the maximum number of iterations of the EM algorithm
 	fn_AM : 		(string) the location to save the alignment model
-	
+
 	OUTPUT:
 	AM :			(dictionary) alignment model structure
 	
@@ -25,11 +44,12 @@ def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
 			LM['house']['maison'] = 0.5
 	"""
     AM = {}
-    
     # Read training data
-    
-    
+    print("data_dir : {}".format(train_dir))
+    eng,fre = read_hansard(train_dir,num_sentences)
     # Initialize AM uniformly
+    assert(len(eng)== len(fre))
+
 
     
     # Iterate between E and M steps
@@ -54,14 +74,23 @@ def read_hansard(train_dir, num_sentences):
 	
 	Make sure to read the files in an aligned manner.
 	"""
-    # TODO
+    eng = read_lan_file(train_dir, num_sentences, "e")
+    fre = read_lan_file(train_dir, num_sentences, "f")
+    return eng, fre
 
 def initialize(eng, fre):
     """
 	Initialize alignment model uniformly.
 	Only set non-zero probabilities where word pairs appear in corresponding sentences.
 	"""
-	# TODO
+    AM ={}
+    for i in range(len(eng)):
+        eng_sen = eng[i]
+        fre_sen = fre[i]
+        for j in range(len(eng_sen)):
+
+
+
     
 def em_step(t, eng, fre):
     """
--- A2/code/preplexity.py
+++ A2/code/preplexity.py
@@ -38,5 +38,5 @@ def preplexity(LM, test_dir, language, smoothing = False, delta = 0):
     return pp
 
 #test
-test_LM = lm_train("lm_train_testdir/", "e", "e_temp")
-print(preplexity(test_LM, "lm_train_testdir/", "e"))
+#test_LM = lm_train("../Hansard/Testing/", "e", "e_temp")
+print(preplexity(test_LM, "../Hansard/Testing/", "e"))
